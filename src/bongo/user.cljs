8d315b4f8e6004dce4defc7c29953852d23224c5
awb99 <andreas.wolfgang.bauer@gmail.com>
2019-11-24 19:54:29 -0600

    dependency builder

---
+++ src/bongo/user.cljs
@@ -0,0 +1,15 @@
+(ns bongo.user
+  (:require
+   ;["moment" :as moment]
+   ;[fortune.core :as fortune]
+   ))
+
+
+(defn help []
+  (println "bongo.user help yourself!"))
+
+#_(defn x []
+  (fortune/cookie))
+
+#_(defn m []
+  (moment))
